7edf43b20aa02503d1de85faa56d6bf635c49c3e
giduon <giduon@naver.com>
2026-02-21 12:14:50 +0900

    관리자 페이지 커뮤니티 업데이트

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+﻿import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import { AdminRouteGuard } from "./AdminRouteGuard";
 import { AuthRouteGuard } from "./AuthRouteGuard";
