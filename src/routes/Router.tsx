8f2160d750f8634588d96ccc7e42e1421a898454
xxx0209 <makelotus0209@gmail.com>
2026-02-21 12:42:14 +0900

    라우터 수정

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -36,66 +36,83 @@ import AdminBlogEdit from "../pages/admin/blog/AdminBlogEdit";
 import MyRestaurantRequestListPage from "../pages/register/MyRestaurantRequestListPage";
 import MyRestaurantRequestDetailPage from "../pages/register/MyRestaurantRequestDetailPage";
 import { AuthProvider } from "../pages/common/context/AuthProvider";
-
+import AdminUserEdit from "../pages/admin/user/AdminUserEdit";
+import AdminUserListPage from "../pages/admin/user/AdminUserListPage";
+import AdminUserDetail from "../pages/admin/user/AdminUserDetail";
 export default function Router() {
   return (
     <BrowserRouter>
       <AuthProvider>
         <Routes>
           <Route element={<MainLayout />}>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/board" element={<BoardPage />} />
-          <Route path="/board/write" element={<BoardWrite />} />
-          <Route path="/board/:id" element={<BoardDetail />} />
-          <Route path="/board/edit/:id" element={<BoardEdit />} />
-          <Route path="/blog" element={<BlogPage />} />
-          <Route path="/blog/write" element={<BlogWrite />} />
-          <Route path="/blog/:id" element={<BlogDetail />} />
-          <Route path="/blog/edit/:id" element={<BlogEdit />} />
+            <Route path="/" element={<Homepage />} />
+            <Route path="/board" element={<BoardPage />} />
+            <Route path="/board/write" element={<BoardWrite />} />
+            <Route path="/board/:id" element={<BoardDetail />} />
+            <Route path="/board/edit/:id" element={<BoardEdit />} />
+            <Route path="/blog" element={<BlogPage />} />
+            <Route path="/blog/write" element={<BlogWrite />} />
+            <Route path="/blog/:id" element={<BlogDetail />} />
+            <Route path="/blog/edit/:id" element={<BlogEdit />} />
 
-          {/* 아래는 나중에 추가 */}
-          {<Route path="/map" element={<MapPage />} />}
-          {/* <Route path="/map" element={<MapPage />} /> */}
-          <Route path="/ai" element={<AIRecommendPage />} />
-          <Route path="/auth/signup" element={<SignupPage />} />
-          <Route path="/auth/login" element={<LoginPage />} />
-          <Route path="/auth/me" element={<Me />} />
-          <Route path="/sample" element={<Sample2 />} />
-          <Route path="/restaurant/:id" element={<Restaurant />} />
-          <Route path="/sample3" element={<Sample3 />} />
-          {/* ROLE_USER, ROLE_ADMIN만 접근 가능 */}
-          <Route element={<AuthRouteGuard />}>
-            <Route path="/register" element={<Register />} />
-            <Route
-              path="/register/requests"
-              element={<MyRestaurantRequestListPage />}
-            />
-            <Route
-              path="/register/requests/:id"
-              element={<MyRestaurantRequestDetailPage />}
-            />
-            <Route path="/auth/mypage" element={<MyPage />} />
-          </Route>
-          {/* ROLE_ADMIN만 접근 가능 */}
-          <Route element={<AdminRouteGuard />}>
-            <Route
-              path="/admin/restaurant-requests"
-              element={<RestaurantRequestPage />}
-            />
-            <Route
-              path="/admin/restaurant-requests/:id"
-              element={<RestaurantRequestDetailPage />}
-            />
-            <Route path="/admin/board" element={<AdminBoardPage />} />
-            <Route path="/admin/board/reports" element={<AdminBoardReportPage />} />
-            <Route path="/admin/board/write" element={<AdminBoardWritePage />} />
-            <Route path="/admin/board/edit/:id" element={<AdminBoardEditPage />} />
-            <Route path="/admin/board/:id" element={<AdminBoardDetailPage />} />
-            <Route path="/admin/blog" element={<AdminBlogPage />} />
-            <Route path="/admin/blog/write" element={<AdminBlogWrite />} />
-            <Route path="/admin/blog/edit/:id" element={<AdminBlogEdit />} />
-            <Route path="/admin/blog/:id" element={<AdminBlogDetail />} />
-          </Route>
+            {/* 아래는 나중에 추가 */}
+            {<Route path="/map" element={<MapPage />} />}
+            {/* <Route path="/map" element={<MapPage />} /> */}
+            <Route path="/ai" element={<AIRecommendPage />} />
+            <Route path="/auth/signup" element={<SignupPage />} />
+            <Route path="/auth/login" element={<LoginPage />} />
+            <Route path="/auth/me" element={<Me />} />
+            <Route path="/sample" element={<Sample2 />} />
+            <Route path="/restaurant/:id" element={<Restaurant />} />
+            <Route path="/sample3" element={<Sample3 />} />
+            {/* ROLE_USER, ROLE_ADMIN만 접근 가능 */}
+            <Route element={<AuthRouteGuard />}>
+              <Route path="/register" element={<Register />} />
+              <Route
+                path="/register/requests"
+                element={<MyRestaurantRequestListPage />}
+              />
+              <Route
+                path="/register/requests/:id"
+                element={<MyRestaurantRequestDetailPage />}
+              />
+              <Route path="/auth/mypage" element={<MyPage />} />
+            </Route>
+            {/* ROLE_ADMIN만 접근 가능 */}
+            <Route element={<AdminRouteGuard />}>
+              <Route
+                path="/admin/restaurant-requests"
+                element={<RestaurantRequestPage />}
+              />
+              <Route
+                path="/admin/restaurant-requests/:id"
+                element={<RestaurantRequestDetailPage />}
+              />
+              <Route path="/admin/board" element={<AdminBoardPage />} />
+              <Route
+                path="/admin/board/reports"
+                element={<AdminBoardReportPage />}
+              />
+              <Route
+                path="/admin/board/write"
+                element={<AdminBoardWritePage />}
+              />
+              <Route
+                path="/admin/board/edit/:id"
+                element={<AdminBoardEditPage />}
+              />
+              <Route
+                path="/admin/board/:id"
+                element={<AdminBoardDetailPage />}
+              />
+              <Route path="/admin/blog" element={<AdminBlogPage />} />
+              <Route path="/admin/blog/write" element={<AdminBlogWrite />} />
+              <Route path="/admin/blog/edit/:id" element={<AdminBlogEdit />} />
+              <Route path="/admin/blog/:id" element={<AdminBlogDetail />} />
+              <Route path="/admin/user" element={<AdminUserListPage />} />
+              <Route path="/admin/user/:id/edit" element={<AdminUserEdit />} />
+              <Route path="/admin/user/:id" element={<AdminUserDetail />} />
+            </Route>
           </Route>
         </Routes>
       </AuthProvider>
